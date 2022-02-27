401c49b4bb48731512b16e1003cf909cdcdfcc93
arusnac <p4k.man@gmail.com>
2022-02-26 17:55:47 -0800

    edit

---
+++ hashtable.py
@@ -0,0 +1,32 @@
+class HashTable(object):
+    def __init__(self, length=4):
+        self.array = [None] * length
+
+    def hash(self, key):
+        """get index of array for specific key"""
+        length = len(self.array)
+        return hash(key) % length
+
+    def insert(self, key, value):
+        index = self.hash(key)
+        if self.array[index] is not None:
+            for kvp in self.array[index]:
+                if kvp[0] == key:
+                    kvp[1] = value
+                    break
+                else:
+                    self.array[index].append([key, value])
+        else:
+            self.array[index] = []
+            self.array[index].append([key, value])
+
+    def get(self, key):
+        index = self.hash(key)
+        if self.array[index] is None:
+            raise KeyError()
+        else:
+            for kvp in self.array[index]:
+                if kvp[0] == key:
+                    return kvp[1]
+
+            raise KeyError()
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import hashtable
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+hashtable.HashTable
 
+hash = hashtable.HashTable()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+hash.insert("test", "1")
+hash.insert("af", "2")
+hash.insert("bb", "3")
+print(hash.get("test") + hash.get("af"))
